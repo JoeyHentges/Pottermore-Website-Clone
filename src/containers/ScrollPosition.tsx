6b268492146c4d7c4c97805ba7bcd40bf71e2626
Joe Hentges <30447791+JoeyHentges@users.noreply.github.com>
2021-10-20 09:05:17 -0500

    simplified scroll position comp - 1

--- src/containers/ScrollPosition.tsx
+++ src/containers/ScrollPosition.tsx
@@ -1,48 +1,58 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 
 import { ScrollPosition } from '../components';
+import { usePreviousState, useWheelListener } from '../hooks';
 
 interface ScrollPositionContainerProps {
   color?: string;
   numberPositions: number;
-  scrollPosition: number;
-  onPositionClicked: (index: number) => void;
+  onScrollPositionChange: (index: number) => void;
 }
 
 /*
   Example Usage:
   {
-    const wheelListener = useWheelListener();
-    const prevWheelListener = usePreviousState(wheelListener);
-
     const [scrollPosition, setScrollPosition] = useState(1);
     const numberScrollPositions = 4;
 
     useEffect(() => {
-      if (!prevWheelListener) return;
-      if (wheelListener.changed === prevWheelListener.changed) return;
-
-      if (wheelListener.goingUp) {
-        scrollPosition > 1 && setScrollPosition(scrollPosition-1) 
-      } else {
-        scrollPosition < numberScrollPositions && setScrollPosition(scrollPosition+1);
-      }
-    }, [wheelListener])
+      console.log("scroll positon changed ", scrollPosition);
+    }, [scrollPosition])
 
     return (
       <>
-        <ScrollPosition numberPositions={numberScrollPositions} scrollPosition={scrollPosition} />
+        <ScrollPosition numberPositions={numberScrollPositions} onScrollPositionChange={setScrollPosition} />
       </>
     );
   }
 */
 
-const ScrollPositionContainer: React.FC<ScrollPositionContainerProps> = ({color, numberPositions, scrollPosition, onPositionClicked}) => {
+const ScrollPositionContainer: React.FC<ScrollPositionContainerProps> = ({color, numberPositions, onScrollPositionChange}) => {
+
+  const wheelListener = useWheelListener();
+  const prevWheelListener = usePreviousState(wheelListener);
+
+  const [scrollPosition, setScrollPosition] = useState(1);
+
+  useEffect(() => {
+    if (!prevWheelListener) return;
+    if (wheelListener.changed === prevWheelListener.changed) return;
+
+    if (wheelListener.goingUp) {
+      scrollPosition > 1 && setScrollPosition(scrollPosition-1) 
+    } else {
+      scrollPosition < numberPositions && setScrollPosition(scrollPosition+1);
+    }
+  }, [wheelListener])
+
+  useEffect(() => {
+    onScrollPositionChange(scrollPosition);
+  }, [scrollPosition])
 
   const getPositionContainers = () => {
     const array = [];
     for (let i = 0; i < numberPositions; i += 1) {
-      array.push(<ScrollPosition.PositionContainer key={`scroll-position-${i}`} active={i+1 === scrollPosition} onClick={() => onPositionClicked(i+1)} />)
+      array.push(<ScrollPosition.PositionContainer key={`scroll-position-${i}`} active={i+1 === scrollPosition} onClick={() => setScrollPosition(i+1)} />)
     }
     return array;
   }
--- src/routes/Home.tsx
+++ src/routes/Home.tsx
@@ -2,32 +2,20 @@ import React, { useEffect, useState } from 'react';
 import { Helmet } from 'react-helmet';
 import { connect } from 'react-redux';
 
-import { useWheelListener, usePreviousState } from '../hooks';
-
 import { ScrollPosition } from '../containers';
 
 const Home: React.FC<{}> = () => {
-  const wheelListener = useWheelListener();
-  const prevWheelListener = usePreviousState(wheelListener);
-
   const [scrollPosition, setScrollPosition] = useState(1);
   const numberScrollPositions = 4;
 
   useEffect(() => {
-    if (!prevWheelListener) return;
-    if (wheelListener.changed === prevWheelListener.changed) return;
-
-    if (wheelListener.goingUp) {
-      scrollPosition > 1 && setScrollPosition(scrollPosition-1) 
-    } else {
-      scrollPosition < numberScrollPositions && setScrollPosition(scrollPosition+1);
-    }
-  }, [wheelListener])
+    console.log("scroll positon changed ", scrollPosition);
+  }, [scrollPosition])
 
   return (
     <>
       <Helmet title="Home" />
-      <ScrollPosition numberPositions={numberScrollPositions} scrollPosition={scrollPosition} onPositionClicked={setScrollPosition} />
+      <ScrollPosition numberPositions={numberScrollPositions} onScrollPositionChange={setScrollPosition} />
       <h1>Home</h1>
     </>
   );
